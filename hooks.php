e13af10d32fd021a369b7bf202cb8173190522b1
Teemu Grönqvist <teemu.gronqvist@net9.fi>
2019-02-09 18:06:09 +0100

    feat(registration): validate additional fields
    Validate the required additional fields and given an error message if
    for example a domestic person does not supply a valid ID number and so
    on

--- hooks.php
+++ hooks.php
@@ -4,6 +4,46 @@ if(!defined('WHMCS'))
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
+add_hook('ShoppingCartValidateDomainsConfig', 50, function ($vars)
+{
+    global $smarty;
+    $errors = [];
+
+    foreach($_SESSION['cart']['domains'] as $key => $domain) {
+        if(isset($vars['domainfield'][$key]['registrant_type']) &&
+            (substr($domain['domain'], -strlen('.fi')) === '.fi')) {
+            switch($vars['domainfield'][$key]['registrant_type']) {
+                case '00':
+                    if(!$vars['domainfield'][$key]['idNumber']) {
+                        $errors[] = 'ID number is required for Finnish residents';
+                    }
+                    break;
+                case '0':
+                    if(!$vars['domainfield'][$key]['birthdate']) {
+                        $errors[] = 'Birth date is required for foreign private persons';
+                    }
+                    break;
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                    if(!$vars['domainfield'][$key]['registerNumber']) {
+                        $errors[] = 'VAT/Register number is a required field for corporate bodies';
+                    }
+                    break;
+                default:
+                    $errors[] = 'Registrant type is missing';
+                    break;
+            }
+        }
+    }
+
+    return $errors;
+});
+
 add_hook('ClientAreaPage', 1, function ($vars)
 {
     if($vars['templatefile'] !== 'configuredomains')
