3050bd1f0fa86d60837bf11edc3ba5af1702a440
Teemu Grönqvist <teemu.gronqvist@net9.fi>
2019-05-17 18:06:08 +0200

    style(translation): double quotes to single quotes

--- lang/finnish.php
+++ lang/finnish.php
@@ -1,7 +1,7 @@
 <?php
-$_LANG['ficora_registrant_type'] = "Rekisteröijän tyyppi";
-$_LANG['Finnish Private Person'] = "Suomalainen yksityishenkilö";
-$_LANG['Foreign Private Person'] = "Ulkomaalainen yksityishenkilö";
+$_LANG['ficora_registrant_type'] = 'Rekisteröijän tyyppi';
+$_LANG['Finnish Private Person'] = 'Suomalainen yksityishenkilö';
+$_LANG['Foreign Private Person'] = 'Ulkomaalainen yksityishenkilö';
 $_LANG['Company'] = 'Yritys';
 $_LANG['Corporation'] = 'Yhditys';
 $_LANG['Institution'] = 'Säätiö';
@@ -14,7 +14,7 @@ $_LANG['ficora_fi_idnumber'] = 'Henkilötunnus <sup style="cursor:help;" title="
 $_LANG['ficora_fi_registernumber'] = 'Y-Tunnus <sup style="cursor:help;" title="Vain yrityksille; yrityksen Y-tunnus tai ulkomaalaisen yrityksen tapauksessa yrityksen EU VAT -tunnus">mikä tämä on?</sup>';
 $_LANG['ficora_fi_birthdate'] = 'Syntymäaika (Käytä muotoa YYYY-KK-PP) <sup style="cursor:help;" title="Vaaditaan ulkomaalaiselta henkilöiltä, jotka eivät asu Suomessa">mikä tämä on?</sup>';
 
-$_LANG['ID number is required for Finnish residents'] = "Henkilötunnus vaadittu Suomessa asuvalle";
-$_LANG['Birth date is required for foreign private persons'] = "Syntymäaika on vaadittu ulkomaalaisilta henkilöiltä";
-$_LANG['VAT/Register number is a required field for corporate bodies'] = "Y-tunnus on vaadittu";
-$_LANG['Registrant type is missing'] = "Rekisteröijän tyyppi puuttuu";
+$_LANG['ID number is required for Finnish residents'] = 'Henkilötunnus vaadittu Suomessa asuvalle';
+$_LANG['Birth date is required for foreign private persons'] = 'Syntymäaika on vaadittu ulkomaalaisilta henkilöiltä';
+$_LANG['VAT/Register number is a required field for corporate bodies'] = 'Y-tunnus on vaadittu';
+$_LANG['Registrant type is missing'] = 'Rekisteröijän tyyppi puuttuu';
